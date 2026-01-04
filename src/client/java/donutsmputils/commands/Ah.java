009281815bb807ada71ef29c081ffdc7910b3c6b
AylexCODE <130986014+AylexCODE@users.noreply.github.com>
2026-01-05 01:51:42 +0800

    Working ah screen with gui

--- src/client/java/donutsmputils/commands/Ah.java
+++ src/client/java/donutsmputils/commands/Ah.java
@@ -6,6 +6,8 @@ import java.util.concurrent.CompletableFuture;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 
+import donutsmputils.components.Auction.AuctionScreen;
+import donutsmputils.utils.AuctionData;
 import donutsmputils.utils.ConfigManager;
 import donutsmputils.utils.RequestData;
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
@@ -25,6 +27,7 @@ public class Ah {
         MinecraftClient.getInstance().player.sendMessage(Text.of("Getting Your Action House Data..."), false);
         CompletableFuture.runAsync(() -> {
             try {
+                ArrayList<AuctionData> data = RequestData.getAuctionData("", "", false);
                 // ArrayList<String> data = RequestData.getAuctionData("", "", false);
 
                 // MinecraftClient client = MinecraftClient.getInstance();
@@ -35,6 +38,8 @@ public class Ah {
                 //         }
                 //     }
                 // });
+                MinecraftClient client = context.getSource().getClient();
+                client.send(() -> client.setScreen(new AuctionScreen(data)));
             }catch(Exception e){
                 if(MinecraftClient.getInstance() != null) MinecraftClient.getInstance().player.sendMessage(Text.of("An error occurred: " + e.getMessage()), false);
             }
---
+++ src/client/java/donutsmputils/components/Auction/AhScreen.java
@@ -0,0 +1,81 @@
+package donutsmputils.components.Auction;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import donutsmputils.utils.AuctionData;
+import net.minecraft.client.gl.RenderPipelines;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+import net.minecraft.util.Identifier;
+
+public class AhScreen extends Screen {
+    private static final Identifier TEXTURE = Identifier.of("donutsmp-utilities", "textures/gui/inventory_small.png");
+    private static final int ELEMENT_HEIGHT = 20;
+    private static final int ELEMENT_SPACING = 10;
+    private ArrayList<AuctionData> data;
+
+    public AhScreen(ArrayList<AuctionData> data){
+        super(Text.of("AuctionScreen"));
+        this.data = data;
+    }
+
+    public void init(){
+        initCloseButton();
+    }
+
+    public void initCloseButton(){
+        int closeButtonWidth = 100;
+        int closeButtonX = width - closeButtonWidth - ELEMENT_SPACING;
+        int closeButtonY = ELEMENT_HEIGHT - ELEMENT_SPACING;
+        ButtonWidget closeButton = ButtonWidget.builder(Text.of("Close"), button -> client.setScreen(null))
+            .dimensions(closeButtonX, closeButtonY, closeButtonWidth, ELEMENT_HEIGHT)
+            .build();
+        
+        addDrawableChild(closeButton);
+    }
+
+    @Override
+    public void render(DrawContext context, int mouseX, int mouseY, float delta) {
+        super.render(context, mouseX, mouseY, delta);
+
+        int invWidth = 176;
+        int invHeight = 77;
+        int invX = (this.width - invWidth) / 2;
+        int invY = (this.height - invHeight) / 2;
+        context.drawTexture(RenderPipelines.GUI_TEXTURED, TEXTURE, invX, invY, 0, 0, invWidth, invHeight, invWidth, invHeight);
+        
+        int y = (this.height - invHeight) / 2 + 17;
+        int x = (this.width - invWidth) / 2 + 8;
+        int size = 16;
+        int slotNumber = 1;
+
+        context.drawText(this.textRenderer, Text.literal("My Auction House Items"), x, y - 11, 0xFF444444, false);
+
+        for(AuctionData item : data){
+            context.drawItem(item.getItemData(), x, y);
+            context.drawStackOverlay(this.textRenderer, item.getItemData(), x, y, null);
+
+            if (mouseX >= x && mouseX <= x + 16 && mouseY >= y && mouseY <= y + 16) {
+                List<Text> customLines = List.of(
+                    Text.literal(item.getItemData().getName().getString()).formatted(Formatting.WHITE),
+                    Text.literal("Price: ").append(Text.literal(item.getItemPrice()).formatted(Formatting.GREEN)),
+                    Text.literal("Time Left: ").append(Text.literal(item.getItemTimeLeft()).formatted(Formatting.GREEN))
+                );
+                context.drawTooltip(this.textRenderer, customLines, mouseX, mouseY);
+                context.fill(x, y, x + size, y + size, 0x70FFFFFF);
+                // context.drawItemTooltip(this.textRenderer, itemStack , mouseX, mouseY);
+            }
+
+            x += 18; slotNumber++;
+            if(slotNumber > 9){
+                x = (this.width - invWidth) / 2 + 8;
+                y += 18;
+                slotNumber = 1;
+            }
+        }
+    }
+}
